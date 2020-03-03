737a1c49edec723deb64a26713d8a2a4fff504c1
Adam Diallo <adam_diallo@live.fr>
2020-03-03 16:24:09 -0500

    Update readMoisture.py

--- Sensors/readMoisture.py
+++ Sensors/readMoisture.py
@@ -1,27 +1,20 @@
-import spidev
-import os
-import time
+from numpy import interp 
+from time import sleep 
 
-delay = 0.2
+spi = spidev.SpiDev() 
+spi.open(0,0)
 
-spi = spidev.SpiDev()
-spi.open(0, 0)
-
-
-def readChannel(channel):
-    val = spi.xfer2([1, (8 + channel) << 4, 0])
-    data = ((val[1] & 3) << 8) + val[2]
-    return data
-
-
-if __name__ == "__main__":
-    try:
-        while True:
-            val = readChannel(0)
-            if (val != 0):
-                print(val)
-            time.sleep(delay)
-
-    except KeyboardInterrupt:
-        print
-        "Cancel."
+def analogInput(channel):
+        spi.max_speed_hz = 1350000
+        adc = spi.xfer2([1,(8+channel)<<4,0])
+       # print("raw data ",adc)
+        data = ((adc[1]&3)<<8)+adc[2]
+        return data 
+        
+        
+while True:
+        output = analogInput(0) 
+        output = interp(output,[0,1023],[100,0])
+        output = int(output)
+        print("Moisture: ", output)
+        sleep(0.1)
