c53b7a166deb277177216fba038af5d7b8d43df6
Adaamm <adam_diallo@live.fr>
2020-03-03 13:24:44 -0500

    changed readMoisture to use SPI instead of I2C

--- Sensors/readMoisture.py
+++ Sensors/readMoisture.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO
+
+import spidev
+import os
 import time
- 
-#GPIO SETUP
-channel = 21
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN)
- 
-def callback(channel):
-        if GPIO.input(channel):
-                print "Water Detected!"
-        else:
-                print "Water Detected!"
- 
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
-GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
- 
-# infinite loop
-while True:
-        time.sleep(1)
+
+delay = 0.2
+
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+
+def readChannel(channel):
+    val = spi.xfer2([1, (8 + channel) << 4, 0])
+    data = ((val[1] & 3) << 8) + val[2]
+    return data
+
+
+if __name__ == "__main__":
+    try:
+        while True:
+            val = readChannel(0)
+            if (val != 0):
+                print(val)
+            time.sleep(delay)
+
+    except KeyboardInterrupt:
+        print("Cancel.")
