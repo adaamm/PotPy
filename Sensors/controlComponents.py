3b30da795a095244537780011cff82eefce84fc7
Adaamm <adam_diallo@live.fr>
2020-03-04 16:39:49 -0500

    pump has been added and it works with the moisture sensor

---
+++ Sensors/controlComponents.py
@@ -0,0 +1,8 @@
+import components
+
+# Just for testingpurposes 
+# print(components.dry())
+while True : 	
+	while (components.dry() == True):
+		components.watering()
+			
--- Sensors/testSensors.py
+++
@@ -1,8 +0,0 @@
-import readMoisture 
-import readLighting
-
-# To test the moisture sensor, uncomment the followingline
-print(readMoisture.level())
-
-# To test the UV Sensor, uncomment the following line
-print(readLighting.level()) 
