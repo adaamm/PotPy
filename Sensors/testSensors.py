fb0ed05caedc9b24c379451108847c1aaf8386e5
Adaamm <adam_diallo@live.fr>
2020-03-03 21:27:35 -0500

    Moisture sensor and UV light sensors are both setup

---
+++ Sensors/testSensors.py
@@ -0,0 +1,8 @@
+import readMoisture 
+import readLighting
+
+# To test the moisture sensor, uncomment the followingline
+# print(readMoisture.level())
+
+# To test the UV Sensor, uncomment the following line
+print(readLighting.level()) 
