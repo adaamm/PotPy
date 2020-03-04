45d4163cdde332bc04597a0615d7b681c391e623
Adaamm <adam_diallo@live.fr>
2020-03-03 20:40:12 -0500

    Use readMoisture.level() to readdata from Moisture sensor, testSensors.py can be used for testing purposes

--- Sensors/readMoisture.py
+++ Sensors/readMoisture.py
@@ -1,10 +1,17 @@
-from numpy import interp 
+# This code is used to read the Moisture sensor's data 
+# The data output by the moisture sensor is from 0 to 1023 where 1023 means complete dryness
+
+
+from numpy import interp # For linear interpolation
 from time import sleep 
+import spidev 
 
-spi = spidev.SpiDev() 
-spi.open(0,0)
+# py-spidev provides a python module for interfacing with SPI devices 
+# Get py-spidev from git clone https://github.com/doceme/py-spidev.git     
+spi = spidev.SpiDev()  
+spi.open(0,0)                                                     
 
-def analogInput(channel):
+def analogInput(channel):          #readchannel
         spi.max_speed_hz = 1350000
         adc = spi.xfer2([1,(8+channel)<<4,0])
        # print("raw data ",adc)
@@ -12,9 +19,11 @@ def analogInput(channel):
         return data 
         
         
-while True:
+def level():
         output = analogInput(0) 
-        output = interp(output,[0,1023],[100,0])
-        output = int(output)
-        print("Moisture: ", output)
-        sleep(0.1)
+        # Note that uncommenting the following line will change the range of numbers outputted
+        #output = interp(output,[0,1023],[100,0]) # interpolate only if needed 
+        moisture = int(output)
+        # Uncomment following line to test Moisture Sensor (with water or soil)
+        # print("Moisture: ", output)
+        return moisture; 
