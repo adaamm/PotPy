aecb2242bc66bf7156c8d27871c3637b7a92996e
Adaamm <adam_diallo@live.fr>
2020-03-20 11:27:49 -0400

    Latest Stable Version of the app

---
+++ PotPy-app/app/src/main/java/com/example/a390application/InsertPlant/Config.java
@@ -0,0 +1,26 @@
+
+package com.example.a390application.InsertPlant;
+
+public class Config {
+
+    public static final String DATABASE_NAME = "Plants-db";
+    public static final int DATABASE_VERSION = 1;
+
+    public static final String PLANT_TABLE_NAME = "plants";
+
+    public static final String COLUMN_PLANT_ID = "_id";
+    public static final String COLUMN_PLANT_NAME = "name";
+    public static final String COLUMN_PLANT_TYPE = "type";
+    public static final String COLUMN_PLANT_MOISTURE = "moisture";
+    public static final String COLUMN_PLANT_LIGHT_INTENSITY = "light";
+    public static final String COLUMN_PLANT_TEST = "test";
+    public static final String COLUMN_PLANT_PH = "ph";
+    public static final String COLUMN_PLANT_TEMPERATURE = "temperature";
+    public static final String COLUMN_PLANT_OWNER = "owner";
+
+    public static final String UNIQUE_ID_TABLE = "userID";
+    public static final String COLUMN_UID_ID = "_id1";
+    public static final String COLUMN_UNIQUE_ID = "uid";
+
+
+}
