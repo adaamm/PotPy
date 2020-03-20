126299241fba815b69de2da468c6c8a649c97906
Adaamm <adam_diallo@live.fr>
2020-03-20 11:47:47 -0400

    Latest Stable Version of the app

---
+++ 390application/app/src/main/java/com/example/a390application/MoreDetailsDialogFragment.java
@@ -0,0 +1,34 @@
+package com.example.a390application;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class MoreDetailsDialogFragment extends DialogFragment {
+
+
+    protected Button doneButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_more_details, container, false);
+
+
+        doneButton = view.findViewById(R.id.backButton);
+
+        doneButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+        return view;
+    }
+}
