04ff57c09619ab370f9ab9a726026c99d12e1d8f
ramidandashli <46937391+ramidandashli@users.noreply.github.com>
2020-03-26 16:46:08 -0400

    Added changes previously done by Tharshana

--- src/main/java/com/example/a390application/inspectPlantActivity.java
+++ src/main/java/com/example/a390application/inspectPlantActivity.java
@@ -6,20 +6,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.a390application.InsertPlant.InsertPlant;
 import com.example.a390application.InsertPlant.Plant;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.List;
 
 public class inspectPlantActivity extends AppCompatActivity {
 
     protected TextView plantsTitle;
+    protected TextView plantsType;
+    protected TextView plantsInfo;
     protected ListView plantsListView;
     protected FloatingActionButton moreDetailsButton;
     protected Button deletePlantButton;
     protected Bundle bundle = new Bundle();
     protected long givenID;
+    protected double plantsIdealMoisture;
+    protected String plantInfo;
+    protected String plantType;
 
 
 
@@ -47,10 +55,23 @@ public class inspectPlantActivity extends AppCompatActivity {
 
 
         plantsTitle = findViewById(R.id.plantName);
+        plantsType = findViewById(R.id.plantType);
+        plantsInfo  = findViewById(R.id.plantInfo);
+
+
         plantsTitle.setText(givenPlant.getName());
+        plantsType.setText(givenPlant.getType());
 
+        plantType = plantsType.getText().toString();
 
-        plantsListView = findViewById(R.id.plantsListView);
+        plantsIdealMoisture = fetchIdealMoisture(plantType);
+
+        plantInfo = /* "General Information: \n" + fetchInfo(plantsType.getText().toString()) + */
+                "\nIdeal Moisture Level: " + plantsIdealMoisture /* + /"Ideal Light Level: " +
+                fetchIdealLightingLevel(plantsType.getText().toString())*/;
+
+        plantsInfo.setText(plantInfo);
+
 
         bundle.putLong("data", givenPlant.getID());
 
@@ -111,6 +132,36 @@ public class inspectPlantActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+    public double fetchIdealMoisture(String type) {
+        Toast.makeText(getApplicationContext(),type,Toast.LENGTH_SHORT).show();
+        double idealMoisture = -1;
+        if (type.equals("Devil's Ivy"))
+            idealMoisture = -1;
+        else if (type.equals("English Ivy"))
+            idealMoisture = -1;
+        else if (type.equals("Sansevieria"))
+            idealMoisture = 100;
+
+        String ideal;
+        ideal = Double.toString(idealMoisture);
+        Toast.makeText(getApplicationContext(),ideal,Toast.LENGTH_SHORT).show();
+        return idealMoisture;
+
+    }
+/*
+    public double fetchIdealLightingLevel(String type) {
+        double idealMoisture;
+        if (type == "Cactus")
+            idealMoisture = 50;
+        else
+        if (type == "Aloe" )
+            idealMoisture = 75;
+        else
+        if (type == "Sansevieria")
+            idealMoisture = 100;
+        else
+            idealMoisture = 0;
+        return idealMoisture;
+    }*/
 }
-
-
