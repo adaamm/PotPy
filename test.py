fadd942524f71db90d62c1a236b82bac2dc8161e
Philippe Vo <philippe.vo.nam@gmail.com>
2020-04-03 15:41:58 -0400

    sprint3: added a test file

---
+++ test.py
@@ -0,0 +1,64 @@
+ # * author : Philippe Vo 
+ # * date : Mar-07-2020 12:32:55
+ 
+# * Imports
+# 3rd Party Imports
+import time
+# User Imports
+from firebase.firebase_plant_com import FirebasePlantCom
+# from plant.Sensors.sensor_controller import SensorController
+from plant.plant import Plant
+
+# * Template
+"""
+one sentence description
+
+Parameters
+----------
+id : str
+        one sentence description
+
+Returns
+-------
+id : str
+    one sentence description
+"""
+
+# * Code
+def main():
+    """
+    starting point of the code
+    """
+    print("Starting Pot-O-Duino ...")
+    
+    # Sign in 2 Firebase
+    firebaseCOM = FirebasePlantCom()
+
+    # Make Plant and its SensorController
+    # sensorController1 = SensorController(moisture=0, light=1, ph=-1, humidity = 0, temperature = 0)
+
+    plantName = "Users/-M3ObxkDK2mLO3D7Eqns/English"
+    plant0 = Plant(name=plantName, type_="Aloe", sensorController="")
+    
+    # Gather data and send 2 Firebase
+    print("Gathering data from sensors and sending to Firebase ...")
+    try:
+        while True:
+            time.sleep(0.01)
+            # plant0.set_all_data()
+            # plantData = plant0.get_all_data()
+            plantData = {
+                "ph": 1,
+                "light": 1,
+                "moisture": 1,
+                "temperature": 1,
+                "humidity": 1
+            }
+            firebaseCOM.update_plant(plant0.name,plantData)
+            plant0.print_plant()
+
+    except KeyboardInterrupt:
+        print('interrupted!')
+
+if __name__ == '__main__':
+    main()
